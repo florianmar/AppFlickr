ca38299dd241f5c61bb87b31fceb3fe1cae6c6f5
florian marin <florian.marin74350@gmail.com>
2016-11-10 16:56:19 +0100

    ajout app

--- dist/js/app.min.js
+++ dist/js/app.min.js
@@ -1,44 +1,103 @@
 $(document).ready(function() {
 
   $(".button-collapse").sideNav();
-  // Querystring, "tags" search term, comma delimited
-var query = "http://www.flickr.com/services/feeds/photos_public.gne?tags=rugby&format=json";
-
-// Ajax call to retrieve data, strange it calls jsonFlickrFeed, tried
-// get a callback but throws an error
-$.ajax({
-   data:{format: "json"},
-   dataType: "jsonp",
-   url: query
-});
-
-// This function is called once the call is satisfied
-// http://stackoverflow.com/questions/13854250/understanding-cross-domain-xhr-and-xml-data
-jsonFlickrFeed = function (data) {
-    console.log("reached jsonFlickerFeed");
-    console.log(data);
+  // appel de la sidenav
 
 
-    // Now start cycling through our array of Flickr photo details
-    $.each(data.items, function(i,item){
-      console.log(item);
 
-      $('<img />').attr('src', item.media.m).appendTo('#photos')
-      console.log('fsdfd');
+  $('a.btn').click(function(){
+    $('div#photos').empty();
+    var mot = $(this).text(); // je stock le text de l'élément cliquer et le rajoute ds l'url
+    var query = "http://www.flickr.com/services/feeds/photos_public.gne?tags="+ mot + "&format=json";
+
+
+    $.ajax({
+      //tags:$(this).text(),
+      data:{format: "json"},
+      dataType: "jsonp",
+      url: query,
+
 
     });
-};
+
+    jsonFlickrFeed = function (data) {
+      console.log("reached jsonFlickerFeed");
+      console.log(data);
+
+
+      // Now
+      $.each(data.items, function(i,item){
+        console.log(item);
+
+        $('<img />').attr('src', item.media.m).appendTo('#photos')
+        console.log('fsdfd');
+
+      });
+    };
+
+  });//fin de la fonction js
 
 
 
 
-  // $.getJSON("http://www.flickr.com/services/feeds/photos_public.gne?tags=soccer&format=jsonp")
-  // .done(function(datas){
-  //   console.log(datas);
-    // jquery : boucle sur datas each()
+  $('button#envoi').click(function(){
+    //au click du btn envoi
+    console.log('jfgklds');
+    var input = $('input#search').val();
+
+    //je récupère la valeur de l'input
+    console.log(input);
+    //je l'envoi ds l'ul
+    $('<a>',{
+      class: 'btn',
+      text: input,
+    }).appendTo('nav');
+
+
+    $('a.btn').click(function(){
+      $('div#photos').empty();
+      var mot = $(this).text(); // je stock le text de l'élément cliquer et le rajoute ds l'url
+      var query = "http://www.flickr.com/services/feeds/photos_public.gne?tags="+ mot + "&format=json";
+
+
+      $.ajax({
+        //tags:$(this).text(),
+        data:{format: "json"},
+        dataType: "jsonp",
+        url: query,
+
+
+      });
+
+      jsonFlickrFeed = function (data) {
+        console.log("reached jsonFlickerFeed");
+        console.log(data);
+
+
+        // Now
+        $.each(data.items, function(i,item){
+          console.log(item);
+
+          $('<img />').attr('src', item.media.m).appendTo('#photos')
+          console.log('fsdfd');
+
+        });
+
+      };
+
+
+    });//dela fonction js2
+    $('a.button-collapse').sideNav('hide');
+    console.log('balabala');
+
+    console.log('hghghhghg');
+    console.log('lala');
+    $('li.tarace').after('<div class="chip">' + input + '</div>');
+    console.log('hfkjdshlfdlkshfkjlsqhflkdsf');
+    $('input#search').val("");
 
 
 
-    // append()
-    // thumbnailUrl
-});
+  });//fin de la fonction click
+
+});//fin du ready
